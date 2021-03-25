08ec45b6db5fc6232afb9aeb50f8b3eabcac8828
Yann Mainier <yann.mainier@gmail.com>
2021-03-25 22:06:25 +0000

    more interesting tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,18 @@
-import App from './App';
+import ReactDOM from "react-dom";
+import App from "./App";
 
-test("whatever", () => {
-  expect(App).not.toBeNull();
+test("Clicking increments the counter", () => {
+  const container = document.createElement("div");
+  document.body.appendChild(container);
+
+  ReactDOM.render(<App />, container);
+
+  expect(container.querySelector("p").textContent).toBe("Nb click: 0");
+
+  const increment = container.querySelector("button");
+  increment.click();
+
+  expect(container.querySelector("p").textContent).toBe("Nb click: 1");
+
+  container.remove();
 });
