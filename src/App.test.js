34b0ac00fa7a550c46c75c0fa80e5e2faf67ceb5
Yann Mainier <yann.mainier@gmail.com>
2021-03-25 22:21:40 +0000

    tyding up

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,28 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 
+function click(element) {
+  element.dispatchEvent(
+    new MouseEvent("click", { bubbles: true, cancelable: true })
+  );
+}
+
 test("Clicking increments the counter", () => {
   const container = document.createElement("div");
   document.body.appendChild(container);
 
   ReactDOM.render(<App />, container);
 
-  expect(container.querySelector("p").textContent).toBe("Nb click: 0");
-
   const increment = container.querySelector("button");
-  increment.click();
+  const paragraph = container.querySelector("p");
 
-  expect(container.querySelector("p").textContent).toBe("Nb click: 1");
+  expect(paragraph.textContent).toBe("Nb click: 0");
+
+  click(increment)
+  expect(paragraph.textContent).toBe("Nb click: 1");
+
+  click(increment)
+  expect(paragraph.textContent).toBe("Nb click: 2");
 
   container.remove();
 });
